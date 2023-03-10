4d3994d0aacb955a40ecdf38a03fd81b8068b387
Anatolii Ivanov <gotnickname@gmail.com>
2023-03-10 14:34:37 +0530

    task4.3.12 solved with an insert sort

--- task4.3.12/Program.cs
+++ task4.3.12/Program.cs
@@ -11,12 +11,24 @@
                 arr[k] -= arr[j];
             }
         }
+        static void InsertSort(int[] arr)
+        {
+            for (int i = 1; i < arr.Length; i++)
+            {
+                int k = i;
+                while (k > 0 && arr[k - 1] > arr[k])
+                {
+                    Swap(arr, k - 1, k);
+                    k--;
+                }
+            }
+        }
+
         static void Main(string[] args)
         {
             var arr = new int[] { 5, 6, 9, 1, 2, 3, 4 };
 
-            //testing Swap
-            Swap(arr, 0, 1);
+            InsertSort(arr);
 
             foreach (int i in arr)
             {
