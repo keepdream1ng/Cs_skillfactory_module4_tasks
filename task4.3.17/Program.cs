3942f7353ac3ecb5dc038b192059dcb72c82687e
Anatolii Ivanov <gotnickname@gmail.com>
2023-03-10 15:16:03 +0530

    task 4.3.17 made skeleton with printarray method

---
+++ task4.3.17/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Cs_skillfactory_module4_tasks
+{
+    internal class Program
+    {
+        static void PrintArray2D(int[,] array)
+        {
+            for (int i = 0; i <= array.GetUpperBound(0); i++)
+            {
+                for (int k = 0; k <= array.GetUpperBound(1); k++)
+                {
+                    Console.Write(array[i, k] + " ");
+                }
+                Console.WriteLine();
+
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int[,] arr = { { -5, 6, 9, 1, 2, -3 }, { -8, 8, 1, 1, 2, -3 } };
+
+            PrintArray2D(arr);
+
+            Console.ReadKey();
+        }
+    }
+}
