dc4560c045cb07ac09d8ff06ebf7904d1e3e6ec5
Anatolii Ivanov <gotnickname@gmail.com>
2023-03-10 20:06:13 +0530

    task 4.3.17 solved

--- task4.3.17/Program.cs
+++ task4.3.17/Program.cs
@@ -11,7 +11,27 @@
                     Console.Write(array[i, k] + " ");
                 }
                 Console.WriteLine();
-
+            }
+        }
+        static void Swap(int[,] arr, int k, int j, int row)
+        {
+            if (k != j)
+            {
+                arr[row, k] += arr[row, j];
+                arr[row, j] = arr[row, k] - arr[row, j];
+                arr[row, k] -= arr[row, j];
+            }
+        }
+        static void InsertSortRow(int[,] arr, int row)
+        {
+            for (int i = 1; i <= arr.GetUpperBound(1); i++)
+            {
+                int k = i;
+                while (k > 0 && arr[row, k - 1] > arr[row, k])
+                {
+                    Swap(arr, k - 1, k, row);
+                    k--;
+                }
             }
         }
 
@@ -20,7 +40,14 @@
             int[,] arr = { { -5, 6, 9, 1, 2, -3 }, { -8, 8, 1, 1, 2, -3 } };
 
             PrintArray2D(arr);
+            Console.WriteLine();
 
+            for (int i = 0; i <= arr.GetUpperBound(0); i++)
+            {
+                InsertSortRow(arr, i);
+            }
+
+            PrintArray2D(arr);
             Console.ReadKey();
         }
     }
