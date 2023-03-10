4f746b8efd569793699cd5714890fd4fd2336a92
Anatolii Ivanov <gotnickname@gmail.com>
2023-03-10 14:05:21 +0530

    buid a skeleton with working swap method

---
+++ task4.3.12/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Cs_skillfactory_module4_tasks
+{
+    internal class Program
+    {
+        static void Swap(int[] arr, int k, int j)
+        {
+            if (k != j)
+            {
+                arr[k] += arr[j];
+                arr[j] = arr[k] - arr[j];
+                arr[k] -= arr[j];
+            }
+        }
+        static void Main(string[] args)
+        {
+            var arr = new int[] { 5, 6, 9, 1, 2, 3, 4 };
+
+            //testing Swap
+            Swap(arr, 0, 1);
+
+            foreach (int i in arr)
+            {
+                Console.Write(i);
+            }
+
+            Console.ReadKey();
+        }
+
+    }
+}
