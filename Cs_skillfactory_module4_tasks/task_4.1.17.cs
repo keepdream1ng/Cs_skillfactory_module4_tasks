ed1ec6f3f66ffc2ea3c7e32be3735de83ba7f335
Anatolii Ivanov <gotnickname@gmail.com>
2023-03-10 10:16:54 +0530

    added cycle end exit

--- Cs_skillfactory_module4_tasks/task_4.1.17.cs
+++ Cs_skillfactory_module4_tasks/task_4.1.17.cs
@@ -6,41 +6,51 @@
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы \nWrite your favorite colour in lower case");
-
-            var color = Console.ReadLine();
-            switch (color)
+            while (true)
             {
+                Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы (stop для остановки)\nWrite your favorite colour in lower case (stop to exit)");
 
-                case "red":
-                    Console.BackgroundColor = ConsoleColor.Red;
-                    Console.ForegroundColor = ConsoleColor.Black;
+                var color = Console.ReadLine();
 
-                    Console.WriteLine("Your color is red!");
-                break;
+                //check if user wants to stop the programm
+                if (color == "stop")
+                {
+                    Console.WriteLine("Цикл остановлен \nCycle stopped");
+                    break;
+                }
+                switch (color)
+                {
 
-                case "green":
-                    Console.BackgroundColor = ConsoleColor.Green;
-                    Console.ForegroundColor = ConsoleColor.Black;
+                    case "red":
+                        Console.BackgroundColor = ConsoleColor.Red;
+                        Console.ForegroundColor = ConsoleColor.Black;
 
-                    Console.WriteLine("Your color is green!");
-                break;
+                        Console.WriteLine("Your color is red!");
+                        break;
 
-                case "Cyan":
-                    Console.BackgroundColor = ConsoleColor.Cyan;
-                    Console.ForegroundColor = ConsoleColor.Black;
+                    case "green":
+                        Console.BackgroundColor = ConsoleColor.Green;
+                        Console.ForegroundColor = ConsoleColor.Black;
 
-                    Console.WriteLine("Your color is cyan!");
-                break;
+                        Console.WriteLine("Your color is green!");
+                        break;
 
-                default: 
-                    Console.BackgroundColor = ConsoleColor.Yellow;
-                    Console.ForegroundColor = ConsoleColor.Red;
+                    case "cyan":
+                        Console.BackgroundColor = ConsoleColor.Cyan;
+                        Console.ForegroundColor = ConsoleColor.Black;
 
-                    Console.WriteLine("Error");
-                break;
+                        Console.WriteLine("Your color is cyan!");
+                        break;
+
+                    default:
+                        Console.BackgroundColor = ConsoleColor.Yellow;
+                        Console.ForegroundColor = ConsoleColor.Red;
+
+                        Console.WriteLine("Error");
+                        break;
+                }
+                Console.ReadKey();
             }
-            Console.ReadKey();
         }
     }
 
