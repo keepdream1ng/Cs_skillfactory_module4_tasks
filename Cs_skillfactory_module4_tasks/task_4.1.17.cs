6e2ec6f06c37b975f9bcf243e20a603e5f40fd2e
Anatolii Ivanov <gotnickname@gmail.com>
2023-03-10 13:21:42 +0530

    now you can type in lowercase

--- Cs_skillfactory_module4_tasks/task_4.1.17.cs
+++ Cs_skillfactory_module4_tasks/task_4.1.17.cs
@@ -8,12 +8,14 @@
         {
             while (true)
             {
-                Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы (stop для остановки)\nWrite your favorite colour in lower case (stop to exit)");
+                Console.WriteLine("Напишите свой любимый цвет на английском (stop для остановки)\nWrite your favorite colour (stop to exit)");
 
                 //task wants me to do a lot of cases, but i want to try and write a shorter version of that code, since we have a user imput
 
                 var text = Console.ReadLine();
                 ConsoleColor choosedColor;
+
+                if (text.Length > 1 && char.IsLower(text[0])) text = char.ToUpper(text[0]) + text.Substring(1);
 
                 //check if user wants to stop the programm
                 if (!ConsoleColor.TryParse(text, out choosedColor))
