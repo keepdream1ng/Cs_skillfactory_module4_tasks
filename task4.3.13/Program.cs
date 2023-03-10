df73f46758d5abd7b134bdb65ab493b6636db765
Anatolii Ivanov <gotnickname@gmail.com>
2023-03-10 14:47:17 +0530

    task 4.3.13 solved

---
+++ task4.3.13/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Cs_skillfactory_module4_tasks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr = new int[] { 5, 6, 9, 1, 2, 3, 4 };
+            int result = 0;
+
+            foreach (var item in arr)
+            {
+                result += item;
+            }
+
+            Console.WriteLine(result);
+
+            Console.ReadKey();
+        }
+    }
+}
