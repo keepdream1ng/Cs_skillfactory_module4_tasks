0d04f1f2c82f470e0930ab8d47f1dc52c0ebcf7d
Anatolii Ivanov <gotnickname@gmail.com>
2023-03-10 15:01:03 +0530

    task 4.3.16 solved with the same code as 4.3.15

---
+++ task4.3.16/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Cs_skillfactory_module4_tasks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] arr = { { -5, 6, 9, 1, 2, -3 }, { -8, 8, 1, 1, 2, -3 } };
+            int result = 0;
+
+            foreach (var item in arr)
+            {
+                if (item > 0) result++;
+            }
+
+            Console.WriteLine(result);
+
+            Console.ReadKey();
+        }
+    }
+}
