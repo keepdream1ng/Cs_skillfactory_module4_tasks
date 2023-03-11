6d4ddf8eccfaaa0ec089be78706b78e5e07e37ec
Anatolii Ivanov <gotnickname@gmail.com>
2023-03-11 05:45:27 +0530

    task 4.5 up to 4.5.5

---
+++ task4.5/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace skillfactory_module4_tasks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            (string Name, string LastName, string Login, int LoginLength, bool HasPet, string[] favcolors, double Age) User;
+
+            Console.WriteLine("Введите имя");
+            User.Name = Console.ReadLine();
+
+            Console.WriteLine("Введите фамилию");
+            User.LastName = Console.ReadLine();
+
+            Console.WriteLine("Введите логин");
+            User.Login = Console.ReadLine();
+            User.LoginLength = User.Login.Length;
+
+            Console.WriteLine("Есть ли у вас животные? Да или Нет");
+            User.HasPet = Console.ReadLine() == "Да" ? true : false;
+
+            Console.WriteLine("Введите возраст пользователя");
+            User.Age = double.Parse(Console.ReadLine());
+
+            User.favcolors = new string[3];
+            Console.WriteLine("Введите три любимых цвета пользователя");
+            for (int i = 0; i <= User.favcolors.GetUpperBound(0); i++)
+            {
+                User.favcolors[i] = Console.ReadLine();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
