84c7797fd501961315d67e2f6e0f125f5d321cc3
Anatolii Ivanov <gotnickname@gmail.com>
2023-03-11 06:12:40 +0530

    task 4.5.6 solved

--- task4.5/Program.cs
+++ task4.5/Program.cs
@@ -4,29 +4,34 @@
     {
         static void Main(string[] args)
         {
-            (string Name, string LastName, string Login, int LoginLength, bool HasPet, string[] favcolors, double Age) User;
-
-            Console.WriteLine("Введите имя");
-            User.Name = Console.ReadLine();
-
-            Console.WriteLine("Введите фамилию");
-            User.LastName = Console.ReadLine();
-
-            Console.WriteLine("Введите логин");
-            User.Login = Console.ReadLine();
-            User.LoginLength = User.Login.Length;
-
-            Console.WriteLine("Есть ли у вас животные? Да или Нет");
-            User.HasPet = Console.ReadLine() == "Да" ? true : false;
-
-            Console.WriteLine("Введите возраст пользователя");
-            User.Age = double.Parse(Console.ReadLine());
-
-            User.favcolors = new string[3];
-            Console.WriteLine("Введите три любимых цвета пользователя");
-            for (int i = 0; i <= User.favcolors.GetUpperBound(0); i++)
+            for (int k = 0; k < 3; k++)
             {
-                User.favcolors[i] = Console.ReadLine();
+                (string Name, string LastName, string Login, int LoginLength, bool HasPet, string[] favcolors, double Age) User;
+
+                Console.WriteLine("Введите имя");
+                User.Name = Console.ReadLine();
+
+                Console.WriteLine("Введите фамилию");
+                User.LastName = Console.ReadLine();
+
+                Console.WriteLine("Введите логин");
+                User.Login = Console.ReadLine();
+                User.LoginLength = User.Login.Length;
+
+                Console.WriteLine("Есть ли у вас животные? Да или Нет");
+                User.HasPet = Console.ReadLine() == "Да" ? true : false;
+
+                Console.WriteLine("Введите возраст пользователя");
+                double.TryParse(Console.ReadLine(), out User.Age);
+
+                User.favcolors = new string[3];
+                Console.WriteLine("Введите три любимых цвета пользователя");
+                for (int i = 0; i <= User.favcolors.GetUpperBound(0); i++)
+                {
+                    User.favcolors[i] = Console.ReadLine();
+                }
+
+                // I guess nex time we gonna lern how to send this data somewhere.
             }
 
             Console.ReadLine();
